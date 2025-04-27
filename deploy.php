63236011b99f7d71b562851c625356408e878cfb
Kokon4 <kicodbz1@gmail.com>
2025-04-27 17:27:45 +0200

    Correcions

--- deploy.php
+++ deploy.php
@@ -5,17 +5,19 @@ require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'git@github.com:cipfpbatoi/repositori-b-sic-Kokon4.git');
+set('repository', 'https://github.com/Kokon4/deployAutomatic.git');
+set('git_cache', false);
 
 add('shared_files', ['.env']);
 add('shared_dirs', ['storage']);
-add('writable_dirs', ['storage','bootstrap/cache']);
-// Hosts posar IP de la máquina
+add('writable_dirs', ['storage', 'bootstrap/cache']);
 
-host('3.225.53.211') ->set('remote_user', 'sa04-deployer')
-->set('identity_file', '~/.ssh/id_rsa')
- ->set('deploy_path', '/var/www/es-cipfpbatoi-deployer/html'); 
+// Hosts posar IP de la máquina
+host('3.225.53.211')
+    ->set('remote_user', 'sa04-deployer')
+    ->set('identity_file', '~/.ssh/id_rsa')
+    ->set('deploy_path', '/var/www/es-cipfpbatoi-deployer/html');
 
 after('deploy:failed', 'deploy:unlock');
 
-before('deploy:symlink','artisan:migrate');
+before('deploy:symlink', 'artisan:migrate');
