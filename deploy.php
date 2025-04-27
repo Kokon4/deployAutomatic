41f884e7f09c9bc47331de5282cb32419e53f793
Kokon4 <kicodbz1@gmail.com>
2025-04-27 17:00:47 +0200

    Afegit projecte Laravel complet

--- deploy.php
+++ deploy.php
@@ -5,7 +5,7 @@ require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'https://github.com/cipfpbatoi/projectes-laravel-Kokon4.git');
+set('repository', 'git@github.com:cipfpbatoi/repositori-b-sic-Kokon4.git');
 
 add('shared_files', ['.env']);
 add('shared_dirs', ['storage']);
@@ -13,12 +13,8 @@ add('writable_dirs', ['storage','bootstrap/cache']);
 // Hosts posar IP de la máquina
 
 host('3.225.53.211') ->set('remote_user', 'sa04-deployer')
- ->set('identity_file', '~/.ssh/id_rsa')
+->set('identity_file', '~/.ssh/id_rsa')
  ->set('deploy_path', '/var/www/es-cipfpbatoi-deployer/html'); 
-
-task('build', function(){
-    run('cd{{release_path}} && build');
-});
 
 after('deploy:failed', 'deploy:unlock');
 
