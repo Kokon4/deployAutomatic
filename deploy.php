3dbcf61f95a4d223600a69660ca40de2394118df
Kokon4 <kicodbz1@gmail.com>
2025-06-09 10:15:51 +0200

    deploy.php

--- deploy.php
+++ deploy.php
@@ -1,22 +1,35 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'https://github.com/Kokon4/deployAutomatic.git');
+set('application', 'futbolfemeni');
+set('repository', 'git@github.com:Kokon4/deployAutomatic.git');
 
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
+add('writable_dirs', ['storage', 'bootstrap/cache']);
 
 // Hosts
 
-host('futbolfemeni.ord.ddaw.com')
-    ->set('remote_user', 'deployer')
-    ->set('deploy_path', '~/deployAutomatic');
+host('34.237.244.74')
+    ->setRemoteUser('futboldeployer')
+    ->setHostname('34.237.244.74')
+    ->set('deploy_path', '/var/www/futbolfemeni/html');
+
+task('deploy:push_env', function () {
+    upload('.env', '{{deploy_path}}/shared/.env');
+});
+
+task('reload:php-fpm', function () {
+    run('sudo /etc/init.d/php8.3-fpm restart');
+});
+
+before('deploy:shared', 'deploy:push_env');
 
 // Hooks
-
+after('deploy','reload:php-fpm');
 after('deploy:failed', 'deploy:unlock');
